b64d881628b0ff90b8b2f30b7c42fc9fbb751b73
Patricio Pilco <pattopilco@gmail.com>
2024-10-12 09:27:04 -0500

    create get post bookmark api

--- src/main/java/com/xpoch/bookmarks/BookmarkRepository.java
+++ src/main/java/com/xpoch/bookmarks/BookmarkRepository.java
@@ -3,8 +3,9 @@ package com.xpoch.bookmarks;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    List<Bookmark> findAllByOrderByCreatedAtDesc();
-
+    List<BookmarkDTO> findAllByOrderByCreatedAtDesc();
+    Optional<BookmarkDTO> findBookmarkById(Long id);
 }
